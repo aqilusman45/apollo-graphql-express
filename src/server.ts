a5744c3b37e0bfbfba4cb7d99b376a1435815e46
Usman Aqil <aqilusman45@gmail.com>
2020-07-14 04:02:42 +0500

    fix: imports for server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,21 @@
-import express from 'express'
+import * as express from 'express'
 import { ApolloServer } from 'apollo-server-express'
-import depthLimit from 'graphql-depth-limit'
+import * as depthLimit from 'graphql-depth-limit'
 import { createServer } from 'http'
-import compression from 'compression'
-import cors from 'cors'
+import * as compression from 'compression'
+import * as cors from 'cors'
 import schema from './schema'
 
 const app = express()
 
 const server = new ApolloServer({
   schema,
-  validationRules: [depthLimit(7)]
+  validationRules: [ depthLimit(7) ]
 })
 
 app.use(cors())
 app.use(compression())
-server.applyMiddleware({ app, path: '/grapql' })
+server.applyMiddleware({ app, path: '/graphql' })
 
 const httpServer = createServer(app)
 
